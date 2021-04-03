e3fe9a517e3aaab7a3d1b61f458def7861da12c9
Jesus Perez <jesus.perez.dev@gmail.com>
2021-04-02 20:47:35 -0400

    New problem 1 factory solution

--- Problem1/ToyFactory.cs
+++ Problem1/ToyFactory.cs
@@ -1,28 +1,66 @@
+using System;
+using System.Linq;
+
 namespace Problem1
 {
     public class ToyFactory
     {
-        public static IToy CreateToy(ToyType type, double cost, string description, string name,
-            string manufacturingCompany, int yearOfManufacture, int minimumAgeLimit, int maximumAgeLimit,
-            bool chockingHazard, double weight)
+        public static T CreateToy<T>(double cost, string description, string name, string manufacturingCompany,
+            int yearOfManufacture, int minimumAgeLimit, int maximumAgeLimit, bool chockingHazard, double weight)
+            where T : Toy
         {
-            switch (type)
+            var type = typeof(T);
+            // var type = typeof(Toy).Assembly
+            //     .DefinedTypes
+            //     .FirstOrDefault(c => typeof(Toy).IsAssignableFrom(typeof(T)));
+
+            if (type == null)
             {
-                case ToyType.Car:
-                    return new Car(cost, description, name, manufacturingCompany, yearOfManufacture, minimumAgeLimit,
-                        maximumAgeLimit, chockingHazard, weight);
-                case ToyType.Dollhouse:
-                    return new Dollhouse(cost, description, name, manufacturingCompany, yearOfManufacture, minimumAgeLimit,
-                        maximumAgeLimit, chockingHazard, weight);
-                case ToyType.StuffedAnimal:
-                    return new StuffedAnimal(cost, description, name, manufacturingCompany, yearOfManufacture, minimumAgeLimit,
-                        maximumAgeLimit, chockingHazard, weight);
-                case ToyType.RainbowStacker:
-                    return new RainbowStacker(cost, description, name, manufacturingCompany, yearOfManufacture, minimumAgeLimit,
-                        maximumAgeLimit, chockingHazard, weight);
-                default:
-                    return null;
+                throw new InvalidOperationException($"Unable to locate type: {typeof(T).AssemblyQualifiedName}");
             }
+
+            var constructor = type.GetConstructor(new Type[] {typeof(double), typeof(string), typeof(string), 
+                typeof(string), typeof(int), typeof(int), typeof(int), typeof(bool), typeof(double)
+            });
+
+            if (constructor == null)
+            {
+                throw new InvalidOperationException(
+                    $"Type {typeof(T).AssemblyQualifiedName} does not have a constructor matching the given parameters");
+            }
+
+            // invoke the parameterized constructor on the given object, using the provided parameters
+            return (T) constructor.Invoke(new object[]
+            {
+                cost, description, name, manufacturingCompany, yearOfManufacture, minimumAgeLimit, maximumAgeLimit,
+                chockingHazard, weight
+            });
         }
+
+        // public static IToy CreateToy(ToyType type, double cost, string description, string name,
+        //     string manufacturingCompany, int yearOfManufacture, int minimumAgeLimit, int maximumAgeLimit,
+        //     bool chockingHazard, double weight)
+        // {
+        //     switch (type)
+        //     {
+        //         case ToyType.Car:
+        //             return new Car(cost, description, name, manufacturingCompany, yearOfManufacture, minimumAgeLimit,
+        //                 maximumAgeLimit, chockingHazard, weight);
+        //         case ToyType.Dollhouse:
+        //             return new Dollhouse(cost, description, name, manufacturingCompany, yearOfManufacture,
+        //                 minimumAgeLimit,
+        //                 maximumAgeLimit, chockingHazard, weight);
+        //         case ToyType.StuffedAnimal:
+        //             return new StuffedAnimal(cost, description, name, manufacturingCompany, yearOfManufacture,
+        //                 minimumAgeLimit,
+        //                 maximumAgeLimit, chockingHazard, weight);
+        //         case ToyType.RainbowStacker:
+        //             return new RainbowStacker(cost, description, name, manufacturingCompany, yearOfManufacture,
+        //                 minimumAgeLimit,
+        //                 maximumAgeLimit, chockingHazard, weight);
+        //         default:
+        //             return null;
+        //     }
+        // }
     }
 }
--- TestProblem1/UnitTest1.cs
+++ TestProblem1/UnitTest1.cs
@@ -13,16 +13,18 @@ namespace TestProblem1
         [Test]
         public void ShouldCreateSpecificToy()
         {
-            var car = ToyFactory.CreateToy(ToyType.Car, 10.15, "A toy car", "Vroom vroom car", 
+            var car = ToyFactory.CreateToy<Car>(10.15, "A toy car", "Vroom vroom car",
                 "ACME", 2021, 5, 20, true, 2.3);
-            Assert.NotNull(car as Car);
+            // var car = ToyFactory.CreateToy(ToyType.Car, 10.15, "A toy car", "Vroom vroom car", 
+            //     "ACME", 2021, 5, 20, true, 2.3);
+            Assert.NotNull(car);
         }
         
         [Test]
         public void ShouldCreateSpecificToyAndGetSpecificProperty()
         {
-            var car = ToyFactory.CreateToy(ToyType.Car, 10.15, "A toy car", "Vroom vroom car", 
-                "ACME", 2021, 5, 20, true, 2.3) as Car;
+            var car = ToyFactory.CreateToy<Car>(10.15, "A toy car", "Vroom vroom car", 
+                "ACME", 2021, 5, 20, true, 2.3);
             var speed = car.Speed;
             Assert.NotNull(speed);
         }
@@ -30,8 +32,8 @@ namespace TestProblem1
         [Test]
         public void ShouldCreateSpecificToyAndCheckSpecificMethodWorks()
         {
-            var car = ToyFactory.CreateToy(ToyType.Car, 10.15, "A toy car", "Vroom vroom car", 
-                "ACME", 2021, 5, 20, true, 2.3) as Car;
+            var car = ToyFactory.CreateToy<Car>(10.15, "A toy car", "Vroom vroom car", 
+                "ACME", 2021, 5, 20, true, 2.3);
             var initialSpeed = car.Speed;
             Assert.NotNull(initialSpeed);
             
@@ -43,16 +45,16 @@ namespace TestProblem1
         [Test]
         public void ShouldCreateRainbowStackerThenRemoveRing()
         {
-            var rs = ToyFactory.CreateToy(ToyType.RainbowStacker, 30.33, "Rainbow rings stacked", "Rainbow Stacker", 
-                "ACME", 2021, 1, 5, true, 5) as RainbowStacker;
-            Assert.NotNull(rs);
-            var initialRings = rs.RingCount;
-            Assert.AreEqual(true,rs.InOrderAndComplete);
+            var rainbowStacker = ToyFactory.CreateToy<RainbowStacker>(30.33, "Rainbow rings stacked", "Rainbow Stacker", 
+                "ACME", 2021, 1, 5, true, 5);
+            Assert.NotNull(rainbowStacker);
+            var initialRings = rainbowStacker.RingCount;
+            Assert.AreEqual(true,rainbowStacker.InOrderAndComplete);
             
-            rs.RemoveRing();
-            var newRings = rs.RingCount;
+            rainbowStacker.RemoveRing();
+            var newRings = rainbowStacker.RingCount;
             Assert.Greater(initialRings, newRings);
-            Assert.AreEqual(false, rs.InOrderAndComplete);
+            Assert.AreEqual(false, rainbowStacker.InOrderAndComplete);
         }
     }
 }
